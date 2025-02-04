5bf9817b85917c82416628f6578674a2e44f1671
JoseRamirezV <jr.ramirez.varon@gmail.com>
2025-02-03 21:26:23 -0500

    FRONT: FIX backend_url variable in axiosInstance

--- frontend/src/config/axiosInstance.ts
+++ frontend/src/config/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = `${import.meta.env.VITE_BACKEND_URL}`;
+const baseURL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 
 const axiosInstance = axios.create({
